17fc622674b8664d38eb75cfa06b030a84621637
karthikcodes-18 <theycallhimkarthik@gmail.com>
2026-07-04 23:17:47 +0530

    Create test-cal.py

---
+++ test-cal.py
@@ -0,0 +1,13 @@
+from cal import *
+
+def test_add():
+  assert add(5,5) == 10
+
+def test_sub():
+  assert sub(10,4) == 6
+
+def test_mul():
+  assert mul(5,6) == 30
+
+def test_div():
+  assert div(20,4) == 5
